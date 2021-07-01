bd9266b5cb3e77770ba2631e94c477aa8cc51e3c
MelianMiko <melianmiko@gmail.com>
2021-07-01 21:23:30 +0700

    v0.7.3.1: Use GitHub to check updates; cleanup

--- app_info.py
+++ app_info.py
@@ -1,2 +1,3 @@
-LINK_GITHUB = "https://gitlab.com/melianmiko/mibandpreview"
+LINK_GITHUB = "https://github.com/melianmiko/mibandpreview"
 LINK_WEBSITE = "https://melianmiko.ru/mibandpreview"
+
--- mibandpreview_qt/use_certifi.py
+++
@@ -1,5 +0,0 @@
-import os
-os.environ['REQUESTS_CA_BUNDLE'] = os.path.join(os.getcwd(), 'cacert.pem')
-
-import certifi
-
