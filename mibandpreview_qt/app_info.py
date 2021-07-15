3d2be088604b35ea23da734046c2a785fcc6b055
MelianMiko <melianmiko@gmail.com>
2021-07-15 20:04:16 +0700

    Fixed version number

--- mibandpreview_qt/app_info.py
+++ mibandpreview_qt/app_info.py
@@ -4,7 +4,7 @@ from pathlib import Path
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 SETTINGS_PATH = str(Path.home())+"/.mi_band_preview.json"
 SETTINGS_VER = 4
-APP_VERSION = "v0.7.4"
+APP_VERSION = "v0.7.4.1"
 
 LINK_GITHUB = "https://github.com/melianmiko/mibandpreview"
 LINK_WEBSITE = "https://melianmiko.ru/mibandpreview"
